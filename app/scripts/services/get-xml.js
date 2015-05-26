33fc164952cf18214e66a71833f10aa268436624
fedex <fedesc@gmail.com>
2015-05-26 19:51:32 +0300

    check

--- app/scripts/services/get-xml.js
+++ app/scripts/services/get-xml.js
@@ -1,6 +1,7 @@
 app.service('getXml', function ($http) {
     var test = $http.get('http://31.168.246.211:456/cgi-bin/runtime.cgi?DEVLIST=1|2|3|4|5|6&FORMAT=xml').success(function (response) {
-        $scope.getXml = response;
+        $scope.getXml = response; 
+        console.log('hola');
     });
 
 });
